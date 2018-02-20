dde5d8bc40ecf3da2799a09029d22bfee79e3fd7
exppl <sliwa8@gmail.com>
2018-02-20 23:10:14 +0100

    Button class logic added

--- cvaluebutton.cpp
+++ cvaluebutton.cpp
@@ -1,21 +1,67 @@
 #include "cvaluebutton.h"
 
-CValueButton::CValueButton()
+CValueButton::CValueButton( int m_iPointStepInput, int m_iLoopLimitInput)
 {
-    m_finalValue = 0;
+    m_iPointValue = 0;
+    m_iIterations = 0;
+    m_iPointStep = m_iPointStepInput;
+    m_iIteratonLimit = m_iLoopLimitInput;
+
 }
 
 void CValueButton::AddValue( int a_iValue )
 {
-    m_finalValue+=a_iValue;
+    m_iPointValue+=a_iValue;
 }
 
 void CValueButton::SubstractValue( int a_iValue )
 {
-    m_finalValue+=a_iValue;
+    m_iPointValue-=a_iValue;
 }
 
-void CValueButton::ResetValue()
+void CValueButton::Reset()
 {
-    m_finalValue = 0;
+    ResetPointValue();
+    ResetCurrentIteration();
+}
+
+void CValueButton::ResetPointValue()
+{
+    m_iPointValue = 0;
+}
+
+void CValueButton::ResetCurrentIteration()
+{
+    m_iIterations = 0;
+}
+
+void CValueButton::IncreaseIteration()
+{
+    if ( m_iIterations < m_iIteratonLimit )
+        AddIteration();
+    else
+        ResetCurrentIteration();
+}
+
+void CValueButton::AddIteration()
+{
+    m_iIterations+=1;
+}
+
+void CValueButton::DecreaseIteration()
+{
+    if ( m_iIterations > 0 )
+        SubstractIteration();
+    else
+        SetMaxIterations();
+}
+
+void CValueButton::SubstractIteration()
+{
+    m_iIterations-=1;
+}
+
+void CValueButton::SetMaxIterations()
+{
+    m_iIterations = m_iIteratonLimit;
 }
--- cvaluebutton.h
+++ cvaluebutton.h
@@ -5,11 +5,25 @@
 class CValueButton
 {
 public:
-    int m_finalValue;
-    CValueButton();
+    CValueButton( int m_iPointStepInput, int m_iIteratonLimitInput);
+    int m_iPointValue;
+
+private:
+    int m_iPointStep;
+    int m_iIterations;
+    int m_iIteratonLimit;
     void AddValue( int a_iValue );
     void SubstractValue( int a_iValue );
-    void ResetValue();
+    void Reset();
+    void ResetPointValue();
+    void ResetCurrentIteration();
+    void IncreaseIteration();
+    void AddIteration();
+    void DecreaseIteration();
+    void SubstractIteration();
+
+    void SetMaxIterations();
+
 };
 
 #endif // VALUEBUTTON_H
